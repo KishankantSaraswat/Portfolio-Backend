f5a5e06dcbc4221bcf105683b544e4cfdcb85903
Krishankant Saraswat <krishnkantsaraswat830@gmail.com>
2026-03-07 14:17:06 +0530

    update

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
 // Custom Routes (to be added)
